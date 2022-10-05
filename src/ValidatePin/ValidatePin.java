c153a278a2b63797bfc7133ab37f2d0858b8c89a
TL <113382633+5-le@users.noreply.github.com>
2022-10-05 14:36:55 -0400

    Validate pin

--- src/ValidatePin/ValidatePin.java
+++ src/ValidatePin/ValidatePin.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class ValidatePin {
 
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner (System.in);
+        int pin = 5678;
+        System.out.println("Enter a PIN.");
+        int userPin =in.nextInt();
+        	while (userPin!=pin) {
+        		System.out.println("Invalid PIN. Try again.");
+        		userPin=in.nextInt();
+        	}
+        	
+        	if (userPin==pin) {
+        		System.out.println("PIN correct. Access granted.");
+        	}
     }
 }
