04f8932e6effd92311ef4cd5efc8f86f58f63be4
TL <113382633+5-le@users.noreply.github.com>
2022-10-11 14:36:15 -0400

    attempt

--- src/DisplayMultiples/DisplayMultiples.java
+++ src/DisplayMultiples/DisplayMultiples.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 public class DisplayMultiples {
 
     public static void main(String args[]) {
-       
+    	Scanner in = new Scanner (System.in);
+    	System.out.println("Enter a number.");
+    	int number = in.nextInt();
+   
+    	number = Math.abs(number);
+    	
+       for (int table=number;table>0;table--);{
+     
+    
+	System.out.println(table);
     }
+
 
 }
