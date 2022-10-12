9ed5e71a8a397fe500d9635884cf92d82f84c956
TL <113382633+5-le@users.noreply.github.com>
2022-10-12 14:17:19 -0400

    Display Multiples

--- src/DisplayMultiples/DisplayMultiples.java
+++ src/DisplayMultiples/DisplayMultiples.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class DisplayMultiples {
 
-    public static void main(String args[]) {
-    	Scanner in = new Scanner (System.in);
-    	System.out.println("Enter a number.");
-    	int number = in.nextInt();
-   
-    	number = Math.abs(number);
-    	
-       for (int table=number;table>0;table--);{
-     
-    
-	System.out.println(table);
-    }
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Choose a number:");
+		int number = in.nextInt();
 
+		
+
+		for (int counter = 1; counter <= 12; counter++) {
+
+			System.out.println(+number+"x"+counter+" = "+number*counter);
+
+		}
+
+	}
 
 }
