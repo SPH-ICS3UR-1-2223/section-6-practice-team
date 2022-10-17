91c8501022e4625a4bde03c3a51334082438ceca
TL <113382633+5-le@users.noreply.github.com>
2022-10-17 13:29:13 -0400

    changes

--- src/LoopShape/LoopShape.java
+++ src/LoopShape/LoopShape.java
@@ -1,67 +1,70 @@
 package LoopShape;
 
 public class LoopShape {
-    
-    static void createRectangle(int width, int height){
-        //Draw a Rectangle
-    	//Draw the top
-    	for (int i=0;i<width;i++) {
-    		System.out.print("#");
-    	}
-    	System.out.println();
-    	//Draw the middle
-    	for (int j = 0; j < height-2; j++) {
-			
-    	System.out.print("#");
-    	for (int i=0;i<width-2;i++); {
-    		System.out.print ("   ");
-    	}
-    	if (width>1) {
-    		System.out.println("#");
-    	}else {
-    		System.out.println();
-    	}
-    }
-    	//Draw the bottom
-    	if (height>1) {
-    		for (int i=0;i<width;i++) {
-        		System.out.print("#");
-        	}
-    		
-    	}
-        	System.out.println(); 
-        
-    }
-    
-    static void createTriangle(int leg){
-        //Draw an Isosceles Right Triangle
-    	System.out.println();
-    	
-    	for (int line=1;line<=leg;line++) {
-    		
-    		if (line == 1) {
-    			System.out.println("#");
-    			
-    		}
-    			
-    			else if (line <1) {
-    				String a = ("#");
-    				String b = (" ");
-    				
-    				
-    				System.out.println(a+b+a);
-    				
-    			} 
-    		
-    		
-    		for (line==leg;) {
-    			System.out.println("#");
-    		}
-    		
-    		};
-    	}
-    	
-    	
-    	
-    }
+
+	static void createRectangle(int width, int height) {
+		// Draw a Rectangle
+		// Draw the top
+		for (int i = 0; i < width; i++) {
+			System.out.print("#");
+		}
+		System.out.println();
+		// Draw the middle
+		for (int j = 0; j < height - 2; j++) {
+
+			System.out.print("#");
+			for (int i = 0; i < width - 2; i++)
+				;
+			{
+				System.out.print("   ");
+			}
+			if (width > 1) {
+				System.out.println("#");
+			} else {
+				System.out.println();
+			}
+		}
+		// Draw the bottom
+		if (height > 1) {
+			for (int i = 0; i < width; i++) {
+				System.out.print("#");
+			}
+
+		}
+		System.out.println();
+
+	}
+
+	static void createTriangle(int leg) {
+		// Draw an Isosceles Right Triangle
+	System.out.println(" ");
+	// if line is 1
+	for (int line = 1; line <= leg; line++) {
+
+	if (line ==1) {
+		// print one symbol
+		System.out.println("#");
+	}
+
+	// else if line < leg
+	else if (line < leg);
+		//print one symbol
+	System.out.println("#");
+		//loop line-2 times
+	for (int counter=1;counter<=line-2;counter++) {
+		//print space
+		System.out.println(" ");
+	}
+	
+	//print one symbol and newline
+	System.out.println("#");
+		
+	}
+	//else
+		//loop leg times
+			//print symbol
+
+	}
+	
+}
 
